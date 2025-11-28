aefa4d45249cb00a4625680e185508ea899bc810
hexahedron1 <elongatedsquareorthobicupola@gmail.com>
2025-11-29 00:12:37 +0300

    more splashes

--- index.js
+++ index.js
@@ -16,8 +16,28 @@ const splashes = [
     "Consists of 100% CuSO₄",
     "Join <a href=\"https://discord.gg/vGyZmXGnea\">my discord server</a> i post things there sometimes",
     "i use arch btw",
-    "i wonder if we hypothetically make an LED the size of the sun and power it would it output more light than the sun",
-    "<audio id=\"pibbleshop\" src=\"pibbleshop shopmania bulgaria.mp3\"></audio><button onclick=\"document.getElementById('pibbleshop').play()\">Announcement</button>"
+    "<audio id=\"pibbleshop\" src=\"pibbleshop shopmania bulgaria.mp3\"></audio><button onclick=\"document.getElementById('pibbleshop').play()\">Announcement</button>",
+    "647e Disengaged from workforce",
+    "this website contains no images",
+    "j1407b is not a planet",
+    "who the hell is a block_618 anyway",
+    "sudo pacman -Sybau",
+    "shoutout to the train wheels",
+    "build a donkey!",
+    "who are you",
+    "idk",
+    "i'm running out of splash ideas",
+    "i have oil",
+    "REPOST THIS MESSAGE IF YOU HATE RIVER OPTIC CABLE",
+    "it is so sad he died of sigma",
+    "red spy in base",
+    "R U R' U' R' F R2 U' R' U' R U R' F'",
+    "едрени пени",
+    "man i'm so hungry i could eat a",
+    "half life 3",
+    "cube?????is this ag eometry dash reference>>>>>???????",
+    "GEOGRAPHY SMASH IS BETTER!!!!!!!! <i>ONE MORBILLION EXPLOSIONS</i>",
+    "Кабардино-Балкария is my refridgerator"
 ]
 splashes[0] = "There are <label style='color: 'rgb(85,255,255)'>"+(splashes.length+1)+" total splash messages that can appear here"
 console.log("thorium")
